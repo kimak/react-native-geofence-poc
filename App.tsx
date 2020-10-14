e5c7b331d7801651f8e5d9dcd862adecb858d8ec
Mickael Dumand <mickael@getluko.com>
2020-10-15 00:08:05 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import GeofencingScreen from './Geofence';
 
-export default function App() {
+/* export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+} */
+
+export default GeofencingScreen
 
 const styles = StyleSheet.create({
   container: {
